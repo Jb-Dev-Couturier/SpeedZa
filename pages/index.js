8903f6355f45c1c2be62e1623acbe8840495ebf4
JB-Dev <jibaoke@gmail.com>
2022-08-15 10:09:10 +0200

    sanity setup ok

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import Head from "next/head";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import Services from "../components/Services";
+import { client } from "../lib/client";
 import css from '../styles/Home.module.css';
 
 
-export default function Home() {
+export default function Home({pizzas}) {
   return (
     <Layout>
     <div className={css.container}>
@@ -22,5 +23,16 @@ export default function Home() {
     </div>
     </Layout>
   );
+}
+
+
+export const getServerSideProps = async ()=>{
+  const query = '*[_type == "pizza"]'
+  const pizzas = await client.fetch(query)
+  return{
+    props:{
+      pizzas
+    }
+  }
 }
 

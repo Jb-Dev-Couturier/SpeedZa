49037a42e942c932159be5688810128126fe575a
JB-Dev <jibaoke@gmail.com>
2022-08-16 08:48:09 +0200

    place order sanity init

--- components/OrderModal.jsx
+++ components/OrderModal.jsx
@@ -1,11 +1,22 @@
 import { Modal, useMantineTheme } from '@mantine/core';
+import { useState } from 'react';
 import css from '../styles/OrderModal.module.css';
 
 
 export default  function OrderModal({opened, setOpened, PaymentMethod}) {
     
   const theme = useMantineTheme();
+  const [FormData, setFormData] = useState({})
 
+
+  const handleInput =(e)=>{
+    setFormData({...FormData, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(FormData);
+  }
   const total = typeof window !== 'undefined' && localStorage.getItem('total')
   
     return (
@@ -21,16 +32,36 @@ export default  function OrderModal({opened, setOpened, PaymentMethod}) {
         onClose={() => setOpened(null)}
       >
         {/* Modal content */}
-        <form action="" className={css.formContainer}>
-          <input type="text" name="name" required placeholder="Nom" />
-          <input type="text" name="phone" required placeholder="Votre Numero" />
-          <textarea name='adress'rows={3} placeholder='Votre Adresse' required></textarea>
+        <form className={css.formContainer} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            required
+            placeholder="Nom"
+            onChange={handleInput}
+          />
+          <input
+            type="text"
+            name="phone"
+            required
+            placeholder="Votre Numero"
+            onChange={handleInput}
+          />
+          <textarea
+            name="adress"
+            rows={3}
+            placeholder="Votre Adresse"
+            required
+            onChange={handleInput}
+          ></textarea>
 
           <span>
             Vous payerez <span> {total} €</span> à la livraison
           </span>
 
-          <button type='submit' className='btn'>Passer la commande</button>
+          <button type="submit" className="btn">
+            Passer la commande
+          </button>
         </form>
       </Modal>
     );

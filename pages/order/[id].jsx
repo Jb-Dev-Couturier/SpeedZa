8956ca90c9b1e77f4b9208ff11a5b6f9213e114e
JB-Dev <jibaoke@gmail.com>
2022-08-16 12:44:40 +0200

    order page status css

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -1,26 +1,94 @@
-import {client} from '../../lib/client'
+import { client } from '../../lib/client';
 import Layout from '../../components/Layout';
+import Image from 'next/image';
 import css from '../../styles/Order.module.css';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
+import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import Spinner from '../../assets/spinner.svg';
 
-export const getServerSideProps = async ({params})=>{
-    const query = `*[_type == 'order' && _id == '${params.id}']`
-    const order = await client.fetch(query)
+export const getServerSideProps = async ({ params }) => {
+  const query = `*[_type == 'order' && _id == '${params.id}']`;
+  const order = await client.fetch(query);
 
-    return{
-        props:{
-            order:order[0]
-        }
-    }
-}
+  return {
+    props: {
+      order: order[0],
+    },
+  };
+};
 
-export default function Orders({order}) {
-    return(
-        <Layout>
-            <div className={css.container}>
-                <span className={css.heading}>
-                    Commande en cours
-                </span>
-            </div>
-        </Layout>
-    )
+export default function Orders({ order }) {
+  return (
+    <Layout>
+      <div className={css.container}>
+        <span className={css.heading}>Commande en cours</span>
+        <div className={css.details}>
+          <hr />
+          <div>
+            <span>N° de Commande :</span>
+            <span>{order._id}</span>
+          </div>
+
+          <div>
+            <span>Nom :</span>
+            <span>{order.name}</span>
+          </div>
+          <div>
+            <span>Numero de Telephone :</span>
+            <span>{order.phone}</span>
+          </div>
+          <div>
+            <span>Adresse Livraison :</span>
+            <span>{order.adress}</span>
+          </div>
+          <div>
+            <span>Methode Paiement :</span>
+            <span>
+              {order.method === 0
+                ? 'Reglement en éspèces à la livraison'
+                : 'En Ligne (déjà payer)'}
+            </span>
+          </div>
+          <hr />
+          <div>
+            <span>Total :</span>
+            <span>
+              {order.total}
+              <span style={{ color: 'red' }}> € </span>
+            </span>
+          </div>
+        </div>
+        <div className={css.statusContainer}>
+          <div className={css.status}>
+            <PaymentsIcon className={css.iconMaterials} />
+            <span>Paiments</span>
+            {order.method === 0 ? (
+              <span className={css.pending}> A la livraison</span>
+            ) : (
+              <span className={css.completed}> Deja payer</span>
+            )}
+          </div>
+          <div className={css.status}>
+            <LocalPizzaIcon className={css.iconMaterials} />
+            <span>En cuisson</span>
+            {order.status === 1 &&(
+                <div className={css.spinner}>
+                    <Image src={Spinner} alt=''/>
+                </div>
+            )}
+          </div>
+          <div className={css.status}>
+            <DeliveryDiningIcon className={css.iconMaterials} />
+            <span>En livraison</span>
+          </div>
+          <div className={css.status}>
+            <InventoryIcon className={css.iconMaterials} />
+            <span>Livré</span>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }

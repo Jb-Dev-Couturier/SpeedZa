0b3bd772791224d766cbcc38e5114dca905160d2
JB-Dev <jibaoke@gmail.com>
2022-08-16 11:00:32 +0200

    pages order process init

--- components/OrderModal.jsx
+++ components/OrderModal.jsx
@@ -1,81 +1,80 @@
 import { Modal, useMantineTheme } from '@mantine/core';
 import { useState } from 'react';
-import toast,{Toaster}  from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { createOrder } from '../lib/orderHandler';
 import { useStore } from '../store/store';
 import css from '../styles/OrderModal.module.css';
+import { useRouter } from 'next/router';
 
-
-export default  function OrderModal({opened, setOpened, PaymentMethod}) {
-    
+export default function OrderModal({ opened, setOpened, PaymentMethod }) {
   const theme = useMantineTheme();
-  const [FormData, setFormData] = useState({})
 
+  const router = useRouter();
+  const [FormData, setFormData] = useState({});
 
-  const handleInput =(e)=>{
-    setFormData({...FormData, [e.target.name]: e.target.value})
-  }
+  const handleInput = (e) => {
+    setFormData({ ...FormData, [e.target.name]: e.target.value });
+  };
 
-  const handleSubmit= async (e)=>{
-    e.preventDefault()
-    const id = await createOrder({...FormData, total, PaymentMethod})
-    toast.success('Votre Commande est en Preparation')
-    resetCard()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = await createOrder({ ...FormData, total, PaymentMethod });
+    toast.success('Votre Commande est en Preparation');
+    resetCard();
     {
       typeof window !== 'undefined' && localStorage.setItem('order', id);
     }
-  }
 
-  const resetCard = useStore((state)=>state.resetCard)
-  const total = typeof window !== 'undefined' && localStorage.getItem('total')
+    router.push(`/order/${id}`)
+  };
 
-  
-    return (
-      <Modal
-        overlayColor={
-          theme.colorScheme === 'dark'
-            ? theme.colors.dark[9]
-            : theme.colors.gray[2]
-        }
-        overlayOpacity={0.55}
-        overlayBlur={3}
-        opened={opened}
-        onClose={() => setOpened(null)}
-      >
-        {/* Modal content */}
-        <form className={css.formContainer} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            required
-            placeholder="Nom"
-            onChange={handleInput}
-          />
-          <input
-            type="text"
-            name="phone"
-            required
-            placeholder="Votre Numero"
-            onChange={handleInput}
-          />
-          <textarea
-            name="adress"
-            rows={3}
-            placeholder="Votre Adresse"
-            required
-            onChange={handleInput}
-          ></textarea>
+  const resetCard = useStore((state) => state.resetCard);
+  const total = typeof window !== 'undefined' && localStorage.getItem('total');
 
-          <span>
-            Vous payerez <span> {total} €</span> à la livraison
-          </span>
+  return (
+    <Modal
+      overlayColor={
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[9]
+          : theme.colors.gray[2]
+      }
+      overlayOpacity={0.55}
+      overlayBlur={3}
+      opened={opened}
+      onClose={() => setOpened(null)}
+    >
+      {/* Modal content */}
+      <form className={css.formContainer} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          required
+          placeholder="Nom"
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          name="phone"
+          required
+          placeholder="Votre Numero"
+          onChange={handleInput}
+        />
+        <textarea
+          name="adress"
+          rows={3}
+          placeholder="Votre Adresse"
+          required
+          onChange={handleInput}
+        ></textarea>
 
-          <button type="submit" className="btn">
-            Passer la commande
-          </button>
-        </form>
-        
-      </Modal>
-    );
+        <span>
+          Vous payerez <span> {total} €</span> à la livraison
+        </span>
+
+        <button type="submit" className="btn">
+          Passer la commande
+        </button>
+      </form>
+    </Modal>
+  );
 }
-
